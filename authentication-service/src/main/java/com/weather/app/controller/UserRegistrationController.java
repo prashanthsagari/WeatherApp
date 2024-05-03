42854152316bc052dadd67acabd311c97c52c296
prashanthsagari <prashanthps7013@gmail.com>
2024-05-03 15:56:44 +0530

    Modify controller

--- authentication-service/src/main/java/com/weather/app/controller/UserRegistrationController.java
+++ authentication-service/src/main/java/com/weather/app/controller/UserRegistrationController.java
@@ -6,6 +6,7 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @RestController
 @RequestMapping("/user-profile")
+@CrossOrigin(origins = "*")
 public class UserRegistrationController {
 
 	@Autowired
