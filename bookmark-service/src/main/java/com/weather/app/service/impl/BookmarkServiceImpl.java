9fab522fc126d08f443d7666b2d6f27c776b43d4
prashanthsagari <prashanthps7013@gmail.com>
2024-05-03 15:57:19 +0530

    remove unwanted code

--- bookmark-service/src/main/java/com/weather/app/service/impl/BookmarkServiceImpl.java
+++ bookmark-service/src/main/java/com/weather/app/service/impl/BookmarkServiceImpl.java
@@ -1,31 +1,19 @@
 package com.weather.app.service.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.bson.Document;
-import org.bson.codecs.Codec;
-import org.bson.codecs.configuration.CodecRegistries;
-import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.client.model.Updates;
 import com.weather.app.db.service.MongoDbService;
 import com.weather.app.service.BookmarkService;
-
-import io.jsonwebtoken.io.IOException;
 
 @Service
 public class BookmarkServiceImpl implements BookmarkService {
@@ -90,25 +78,25 @@ public class BookmarkServiceImpl implements BookmarkService {
 		return null;
 	}
 
-	public static void main(String[] args) {
-		BookmarkServiceImpl impl = new BookmarkServiceImpl();
-		impl.abc();
-	}
-
-	public void abc() {
-		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-		MongoDatabase mongoDatabase = mongoClient.getDatabase("favorites_db");
-		MongoCollection<Document> collection = mongoDatabase.getCollection("bookmarks");
-
-		Bson filterQuery = Filters.eq("username", "mno");
-		Document user = collection.find(filterQuery).first();
-
-		Document update = new Document("$pull",
-				new Document("favorites", new Document("id", "2e61210a-4380-419a-b427-51a25c5aaf67")));
-		collection.updateOne(filterQuery, update);
-
-		System.out.println("Document updated successfully.");
-
-	}
+//	public static void main(String[] args) {
+//		BookmarkServiceImpl impl = new BookmarkServiceImpl();
+//		impl.abc();
+//	}
+//
+//	public void abc() {
+//		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+//		MongoDatabase mongoDatabase = mongoClient.getDatabase("favorites_db");
+//		MongoCollection<Document> collection = mongoDatabase.getCollection("bookmarks");
+//
+//		Bson filterQuery = Filters.eq("username", "mno");
+//		Document user = collection.find(filterQuery).first();
+//
+//		Document update = new Document("$pull",
+//				new Document("favorites", new Document("id", "2e61210a-4380-419a-b427-51a25c5aaf67")));
+//		collection.updateOne(filterQuery, update);
+//
+//		System.out.println("Document updated successfully.");
+//
+//	}
 
 }
